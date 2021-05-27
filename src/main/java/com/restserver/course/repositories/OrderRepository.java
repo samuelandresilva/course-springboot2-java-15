2d0d0b80aef2ac178b1749298c1b55aa72f1a29f
Samuel André <samuel@multitecsistemas.com.br>
2021-05-27 13:38:32 -0300

    Order, Instant, ISO 8601 - Part 2

---
+++ src/main/java/com/restserver/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.restserver.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.restserver.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
