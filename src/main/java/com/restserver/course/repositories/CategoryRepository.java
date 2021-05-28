93c13e00d960e2be748571cd9e0004f7e10da17c
Samuel André <samuel@multitecsistemas.com.br>
2021-05-28 13:17:35 -0300

    Category

---
+++ src/main/java/com/restserver/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.restserver.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.restserver.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
