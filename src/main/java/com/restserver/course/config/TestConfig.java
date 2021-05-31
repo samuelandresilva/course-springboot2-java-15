a19ae794346ad6627f56b19a4b66662c6d126b0a
Samuel André <samuel@multitecsistemas.com.br>
2021-05-31 09:11:31 -0300

    Payment, one-to-one association

--- src/main/java/com/restserver/course/config/TestConfig.java
+++ src/main/java/com/restserver/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.restserver.course.entities.Category;
 import com.restserver.course.entities.Order;
 import com.restserver.course.entities.OrderItem;
+import com.restserver.course.entities.Payment;
 import com.restserver.course.entities.Product;
 import com.restserver.course.entities.User;
 import com.restserver.course.entities.enuns.OrderStatus;
@@ -79,6 +80,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 		
 	}
 }
